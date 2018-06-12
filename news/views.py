c5cf40104e7c7e101bdb2459b5129bdc7edccadf
Richard Waweru <developer.waweru@gmail.com>
2018-06-12 18:22:30 +0300

    Adding forms model

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 # Create your views here.
 def welcome(request):
@@ -13,7 +14,14 @@ def news_of_day(request):
     news = Article.today_news()
     news.reverse()
 
-    return render(request, 'all-news/today-news.html',{'date':date, 'news':news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    
+    return render(request, 'all-news/today-news.html',{'date':date, 'news':news, 'letterForm':form})
 
 def convert_dates(dates):
     # Function that gets the weekday number for the date
